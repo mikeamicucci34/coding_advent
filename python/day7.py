5a773d05249a73559a4ebe335a8b08df889b3101
Matteo Santamaria <msantama@gmail.com>
2021-12-07 07:52:29 -0800

    python day 7

---
+++ python/day7.py
@@ -0,0 +1,28 @@
+import statistics
+
+with open("../inputs/day7.txt", "r") as f:
+    data = [int(x) for x in f.read().strip().split(",")]
+
+median = statistics.median(data)
+energy_usage = sum(abs(median - x) for x in data)
+print(f"part one: {energy_usage}")
+
+
+def f(x_i: int, x_0: int) -> int:
+    """Compute total cost of realignment.
+
+    :param
+        x_i: starting point
+        x_0: ending point
+    """
+    z = x_i - x_0
+    return (z ** 2 + abs(z)) / 2
+
+
+cost = float("inf")
+for j in range(min(data), max(data)):
+    test = sum(f(x_i=x, x_0=j) for x in data)
+    if test < cost:
+        cost = test
+
+print(f"part two: {cost}")
